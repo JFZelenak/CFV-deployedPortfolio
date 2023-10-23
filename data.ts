8558bdc4472db82788015ae51bfcd20d4477c26c
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-23 23:44:18 +0200

    add changes

---
+++ data.ts
@@ -0,0 +1,16 @@
+let tools: Array<{
+    name: string,
+    percentage: number
+}> = [{
+    name: "HTML",
+    percentage: 90
+}, {
+    name: "CSS",
+    percentage: 85
+}, {
+    name: "HTML",
+    percentage: 90
+}, {
+    name: "HTML",
+    percentage: 90
+}];
