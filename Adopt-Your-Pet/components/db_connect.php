311a11f4ce347fe426ceea8e352f923699b0bc9d
Johannes Zelenak <jf.zelenak@gmail.com>
2023-12-12 19:01:22 +0100

    add changes

--- Adopt-Your-Pet/components/db_connect.php
+++ Adopt-Your-Pet/components/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "be20_cr5_animal_adoption_johanneszelenak";
+$host = "5.189.178.173";
+$user = "zelenakcodefacto_JFZelenak";
+$pass = "Burg&Auto!Panzer";
+$dbname = "zelenakcodefacto_be20_cr5_animal_adoption_johanneszelenak";
 
 $connect = new mysqli($host, $user, $pass, $dbname);
 
